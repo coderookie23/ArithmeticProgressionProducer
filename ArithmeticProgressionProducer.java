1ef14c279fdd51302eeb0e3f8a8b4f1771f6831c
sup-rabh <ersaurabh@zohomail.in>
2023-05-04 22:26:48 +0530

    The code is an example of multi-threading in Java, which demonstrates the use of a BlockingQueue to communicate between producers and consumers. Two producers generate arithmetic progressions and put them in the queue, while a printer thread reads from the queue and prints the numbers in a LIFO order during random print windows. The program runs for a fixed amount of time and then shuts down.

---
+++ ArithmeticProgressionProducer.java
@@ -0,0 +1,100 @@
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class ArithmeticProgressionProducer implements Runnable {
+    private final BlockingQueue<Integer> queue;
+    private final int start;
+    private final int increment;
+    private final long minIntervalMillis;
+    private final long maxIntervalMillis;
+    private final Random random = new Random();
+    private final AtomicBoolean isPrinting = new AtomicBoolean(false);
+
+    public ArithmeticProgressionProducer(BlockingQueue<Integer> queue, int start, int increment, long minIntervalMillis, long maxIntervalMillis) {
+        this.queue = queue;
+        this.start = start;
+        this.increment = increment;
+        this.minIntervalMillis = minIntervalMillis;
+        this.maxIntervalMillis = maxIntervalMillis;
+    }
+
+    @Override
+    public void run() {
+        int currentValue = start;
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                queue.put(currentValue);
+                currentValue += increment;
+                Thread.sleep(random.nextInt((int) (maxIntervalMillis - minIntervalMillis)) + minIntervalMillis);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BlockingQueue<Integer> queue = new LinkedBlockingQueue<>();
+        ArithmeticProgressionProducer producer1 = new ArithmeticProgressionProducer(queue, 0, 1, 1000, 2000);
+        ArithmeticProgressionProducer producer2 = new ArithmeticProgressionProducer(queue, 100, 2, 1500, 2500);
+        Printer printer = new Printer(queue, 5000, 10000);
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        executor.execute(producer1);
+        executor.execute(producer2);
+        executor.execute(printer);
+
+        // Wait for some time to allow the producers to generate numbers
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Shut down the executor
+        executor.shutdownNow();
+    }
+
+    private static class Printer implements Runnable {
+        private final BlockingQueue<Integer> queue;
+        private final long minPrintWindowMillis;
+        private final long maxPrintWindowMillis;
+        private final AtomicBoolean isPrinting = new AtomicBoolean(false);
+
+        public Printer(BlockingQueue<Integer> queue, long minPrintWindowMillis, long maxPrintWindowMillis) {
+            this.queue = queue;
+            this.minPrintWindowMillis = minPrintWindowMillis;
+            this.maxPrintWindowMillis = maxPrintWindowMillis;
+        }
+
+        @Override
+        public void run() {
+            Deque<Integer> stack = new ArrayDeque<>();
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    if (isPrinting.compareAndSet(false, true)) {
+                        // start printing window
+                        long printWindowMillis = ThreadLocalRandom.current().nextLong(minPrintWindowMillis, maxPrintWindowMillis + 1);
+                        long endTimeMillis = System.currentTimeMillis() + printWindowMillis;
+                        while (System.currentTimeMillis() < endTimeMillis && !queue.isEmpty()) {
+                            Integer num = queue.poll();
+                            if (num != null) {
+                                stack.push(num);
+                            }
+                        }
+                        isPrinting.set(false);
+                        // print in LIFO order
+                        while (!stack.isEmpty()) {
+                            Integer num = stack.pop();
+                            System.out.println(num);
+                        }
+                    } else {
+                        // wait until printing window is over
+                        Thread.sleep(100);
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
